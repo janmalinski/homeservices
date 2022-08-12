95d9b6cb35ad13296c142787560f498471a01316
Jan Malinski <janmalinski@jans-macbook-pro-2.home>
2022-08-12 11:00:13 +0200

    add react-native-fast-image, babel-plugin-module-resolver and modify babel.config.js - fix imports problem

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ['./'],
+        alias: {
+          '@src': './src',
+          '@components': './src/components',
+          '@designSystem': './src/components/designSystem',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ]
 };

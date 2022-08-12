d260300eaf0125602181d5655c99b773ed2e91e4
Jan Malinski <janmalinski@jans-macbook-pro-2.home>
2022-08-12 11:27:53 +0200

    add SafeAreaProvider and StatusBar in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import {RootNavigator} from './navigation/RootNavigator';
+import { RootNavigator } from './navigation/RootNavigator';
 
 const App = () => {
-  return <RootNavigator />;
+  return (
+    <SafeAreaProvider>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="dark-content"
+      />
+      <RootNavigator />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;

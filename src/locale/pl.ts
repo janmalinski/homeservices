750420388750a61135fd76ed7776001f9f99aca6
Jan Malinski <janmalinski@jans-macbook-pro.home>
2022-08-15 11:35:40 +0200

    add language localization and translations, translations need to be finished

---
+++ src/locale/pl.ts
@@ -0,0 +1,41 @@
+const translations = {
+  common: {
+    cancel: 'Anuluj',
+  },
+  signIn: {
+    signIn: 'Zaloguj się',
+  },
+  adList: {
+    contentList: 'Treści',
+  },
+  settings: {
+    settings: 'Ustawienia',
+    account: 'Konto',
+    signOutTitle: 'Wyloguj się',
+    signOutSubtitle: 'Po wylogowaniu się, musisz zalogować się ponownie',
+    signOutAction: 'Wyloguj',
+    signOutDialogTitle: 'Wyloguj się',
+    signOutDialogDescription: 'Czy na pewno chcesz się wylogować?',
+  },
+  account: {
+    account: 'Profil uzytkownika',
+  },
+  location: {
+    howCanWeHelpYou: 'Jak możemy Tobię pomóc?',
+    location: 'Location',
+    clientOrWorker: 'Are you looking for Home Help, or you are Home Help?',
+    client: 'I am looking for Home Help',
+    worker: 'I am Home Help',
+    clientEmploymentType: 'I am looking for home help',
+    workerEmploymentType: 'I am looking for client',
+    yourLocation: 'Your location',
+    detectLocation: 'Detect my location or set location manually',
+  },
+  map: {
+    searchOrMoveTheMap: 'Search or move the map',
+    whereAreYouLookingForHelp: 'Gdzie chcesz znaleźć pomoc domową?',
+    whichAreYouWantToWork: 'W jakim rejonie chcesz pracować?',
+  },
+};
+
+export default translations;

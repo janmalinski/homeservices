ac1c68fc079430631900ebfc03cd0c99c01684ef
Jan Maliński <contact@janmalinski.com>
2023-03-16 10:18:04 +0100

    modify StatusBar in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ import { Provider } from 'react-redux';
 import { RootNavigator } from './navigation/RootNavigator';
 import { store } from './store';
 import './locale/i18nConfig';
+// import { useSendFCMToken } from './utils/hooks/useSendFCMToken';
 
 const App = () => {
+  // useSendFCMToken();
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
         <StatusBar
-          backgroundColor="transparent"
-          translucent
           barStyle="dark-content"
+          translucent
+          backgroundColor="transparent"
         />
         <RootNavigator />
       </SafeAreaProvider>

ab5dd1bd208b8d622b447921412edd5e88f589f2
Jan Maliński <contact@janmalinski.com>
2023-03-29 10:40:07 +0200

    add small changes in chatApi

--- src/Chat/chatApi.ts
+++ src/Chat/chatApi.ts
@@ -1,15 +1,15 @@
 import { protectedApi } from '@src/Api/protectedApi';
 import { ChatDto } from './chat.dto';
 
-type TCheckOrCreateRoom = (adId: string, authorId: string, userId: string) => Promise<string>;
+type TCheckOrCreateRoom = (adId: string, authorId: string, userId: string, roomId?: string) => Promise<{roomId: string; authorId: string; userId: string}>;
 
 type TGetMessages = (roomId: string) => Promise<ChatDto.Message[]>;
 
-type TPostMessage = (text: string, roomId: string, senderId: string,) => Promise<number>;
+type TPostMessage = (text: string, roomId: string, senderId: string,) => Promise<any>;
 
-export const checkOrCreateRoom: TCheckOrCreateRoom = async( adId, authorId, userId) => {
-  const response = await protectedApi.post(`/room/${adId}/${authorId}/${userId}`);
-  return response.data.roomId;
+export const checkOrCreateRoom: TCheckOrCreateRoom = async( adId, authorId, userId, roomId) => {
+  const response = await protectedApi.post(`/room/${adId}/${authorId}/${userId}/${roomId}`);
+  return response.data;
 } 
 
 export const getMessages: TGetMessages = async(roomId: string) => {
@@ -21,5 +21,5 @@ export const postMessage: TPostMessage = async(text, roomId, senderId) => {
   const response = await protectedApi.post(`/room/${roomId}/${senderId}`, {
         text,
       });
-  return response.status;
+  return response;
 };

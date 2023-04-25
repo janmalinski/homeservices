9b3fe1d7948f732335b378ea784b30708661de54
Jan Maliński <contact@janmalinski.com>
2023-04-25 12:14:43 +0200

    add messaging().setBackgroundMessageHandler in index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 /**
  * @format
  */
-
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import messaging from '@react-native-firebase/messaging';
 
-AppRegistry.registerComponent(appName, () => App);
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message is handled in the background', remoteMessage)
+});
+
+const AppContainer = ({isHeadless}) => {
+    if(isHeadless){
+        console.log('App launched by IOS in background, ignore it.')
+        return null;
+    }
+       return  <App/>
+    }
+
+
+AppRegistry.registerComponent(appName, () => AppContainer);

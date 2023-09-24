2f6b2b6dc725092b434574109f274a4bcb5f5bb0
Jan Malinski <janmalinski@macbook-pro-jan.home>
2023-09-24 17:56:03 +0200

    fix gesture handler error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,27 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { RootNavigator } from './navigation/RootNavigator';
 import { store } from './store';
 import './locale/i18nConfig';
 
+
 const App = () => {
 
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <StatusBar
-          barStyle="dark-content"
-          translucent
-          backgroundColor="transparent"
-        />
-        <RootNavigator />
-      </SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaProvider>
+          <StatusBar
+            barStyle="dark-content"
+            translucent
+            backgroundColor="transparent"
+          />
+          <RootNavigator />
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
     </Provider>
   );
 };
